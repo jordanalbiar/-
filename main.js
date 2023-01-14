6e9d8be20564b509e4ef870c3b6768a3afbafc5b
jordanalbiar <jordanalbiar@gmail.com>
2023-01-13 22:08:35 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 
+const input = document.getElementById("terminal-input");
+const output = document.querySelector(".terminal-output");
+
+input.addEventListener("keydown", (event) => {
+  if (event.keyCode === 13) { // 13 is the key code for the Enter key
+    event.preventDefault();
+    const command = input.value;
+    input.value = "";
+    output.innerHTML += `<p>$ ${command}</p>`;
+    // Add code here to handle the command
+  }
+});
